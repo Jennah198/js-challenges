db1aab7d56be5cae3a841f7e71aac643e117b501
Nezira <neziraworku198@gmail.com>
2025-12-01 11:42:48 -0800

    practiced the ternary operator

--- fundamentals/challenge-1.js
+++ fundamentals/challenge-1.js
@@ -18,41 +18,48 @@
 
 // Coding Challenge #3
 
-var dolphinsScore1 = 97
-var dolphinsScore2 = 112
-var dolphinsScore3 = 101
+// var dolphinsScore1 = 97
+// var dolphinsScore2 = 112
+// var dolphinsScore3 = 101
 
-var koalasScore1 = 109
-var koalasScore2 = 95
-var koalasScore3 = 123
+// var koalasScore1 = 109
+// var koalasScore2 = 95
+// var koalasScore3 = 123
 
-var dolphinsAverage = (dolphinsScore1 + dolphinsScore2 + dolphinsScore3) / 3
-var koalasAverage = (koalasScore1 + koalasScore2 + koalasScore3) / 3
+// var dolphinsAverage = (dolphinsScore1 + dolphinsScore2 + dolphinsScore3) / 3
+// var koalasAverage = (koalasScore1 + koalasScore2 + koalasScore3) / 3
 
-if (dolphinsAverage > koalasAverage && dolphinsAverage >= 100) {
-  console.log(
-    `Dolphins win the trophy with an average score of ${dolphinsAverage.toFixed(
-      2
-    )}!`
-  )
-} else if (koalasAverage > dolphinsAverage && koalasAverage >= 100) {
-  console.log(
-    `Koalas win the trophy with an average score of ${koalasAverage.toFixed(
-      2
-    )}!`
-  )
-} else if (
-  dolphinsAverage === koalasAverage &&
-  dolphinsAverage >= 100 &&
-  koalasAverage >= 100
-) {
-  console.log(
-    `It's a draw with both teams having an average score of ${dolphinsAverage.toFixed(
-      2
-    )}!`
-  )
-} else {
-  console.log(
-    'No team wins the trophy as the minimum score of 100 was not met.'
-  )
-}
+// if (dolphinsAverage > koalasAverage && dolphinsAverage >= 100) {
+//   console.log(
+//     `Dolphins win the trophy with an average score of ${dolphinsAverage.toFixed(
+//       2
+//     )}!`
+//   )
+// } else if (koalasAverage > dolphinsAverage && koalasAverage >= 100) {
+//   console.log(
+//     `Koalas win the trophy with an average score of ${koalasAverage.toFixed(
+//       2
+//     )}!`
+//   )
+// } else if (
+//   dolphinsAverage === koalasAverage &&
+//   dolphinsAverage >= 100 &&
+//   koalasAverage >= 100
+// ) {
+//   console.log(
+//     `It's a draw with both teams having an average score of ${dolphinsAverage.toFixed(
+//       2
+//     )}!`
+//   )
+// } else {
+//   console.log(
+//     'No team wins the trophy as the minimum score of 100 was not met.'
+//   )
+// }
+
+// Coding Challenge #4
+let bill = 275
+let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+)
