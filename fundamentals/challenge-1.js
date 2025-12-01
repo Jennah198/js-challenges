23d4be7efb61cd8a601c175879d7ce65e095f7fd
Nezira <neziraworku198@gmail.com>
2025-12-01 11:17:38 -0800

    using string litterals

--- fundamentals/challenge-1.js
+++ fundamentals/challenge-1.js
@@ -7,9 +7,9 @@ var johnWeight = 85
 var markBmi = markWeight / (markHeight * markHeight)
 var johnBmi = johnWeight / (johnHeight * johnHeight)
 
-if ((markHigherBmi = markBmi > johnBmi)) {
-  console.log("Mark's BMI is higher than John's.")
+if (markBmi > johnBmi) {
+  console.log(`Mark's BMI ${markBmi} is higher than John's.`)
 } else {
-  console.log("John's BMI is higher than Mark's.")
+  console.log(`John's BMI ${johnBmi.toFixed(2)} is higher than Mark's.`)
 }
 //console.log("Is Mark's BMI higher than John's? " + markHigherBmi)
