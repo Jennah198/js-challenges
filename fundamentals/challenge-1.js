d4a2cdbfe5ed541717e626d6a1127db0fbabff03
Nezira <neziraworku198@gmail.com>
2025-12-01 11:09:06 -0800

    if else and variable usage practice

--- fundamentals/challenge-1.js
+++ fundamentals/challenge-1.js
@@ -3,3 +3,13 @@ var johnHeight = 1.76
 
 var markWeight = 95 // in kilograms
 var johnWeight = 85
+
+var markBmi = markWeight / (markHeight * markHeight)
+var johnBmi = johnWeight / (johnHeight * johnHeight)
+
+if ((markHigherBmi = markBmi > johnBmi)) {
+  console.log("Mark's BMI is higher than John's.")
+} else {
+  console.log("John's BMI is higher than Mark's.")
+}
+//console.log("Is Mark's BMI higher than John's? " + markHigherBmi)
