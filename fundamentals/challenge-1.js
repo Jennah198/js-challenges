ab9f6950eb6367faf1e2555fa58dca1be3d4381e
Nezira <neziraworku198@gmail.com>
2026-01-28 05:33:28 -0800

    prepared  a setup for a quote generator project

--- fundamentals/challenge-1.js
+++ fundamentals/challenge-1.js
@@ -64,18 +64,35 @@
 //   `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
 // )
 
-const calcAverage = (a, b, c) => (a + b + c) / 3
+// JavaScript Fundamentals – Part 2
+// Coding Challenge #1
+// const calcAverage = (a, b, c) => (a + b + c) / 3
 
-const avgDolphins = calcAverage(44, 23, 71)
-const avgKoalas = calcAverage(65, 54, 49)
-console.log(avgDolphins, avgKoalas)
+// const avgDolphins = calcAverage(44, 23, 71)
+// const avgKoalas = calcAverage(65, 54, 49)
+// console.log(avgDolphins, avgKoalas)
 
-const checkWinner = function (avgDolphins, avgKoalas) {
-  if (avgDolphins >= 2 * avgKoalas) {
-    console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`)
-  } else {
-    console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`)
-  }
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`)
+//   } else {
+//     console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`)
+//   }
+// }
+
+// checkWinner(avgDolphins, avgKoalas)
+
+// Coding Challenge #2
+
+const calcTip = function (bill) {
+  return (tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2)
 }
 
-checkWinner(avgDolphins, avgKoalas)
+const bills = [125, 555, 44]
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+const total = [
+  bills[0] + calcTip(bills[0]),
+  bills[1] + calcTip(bills[1]),
+  bills[2] + calcTip(bills[2]),
+]
+console.log(bills, tips, total)
