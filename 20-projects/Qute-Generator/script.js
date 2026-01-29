6d69341193c34793bde20c3c230313751c6f4707
Nezira <neziraworku198@gmail.com>
2026-01-29 15:42:22 -0800

    added a function to randomely fatch quotes from local storage

---
+++ 20-projects/Qute-Generator/script.js
@@ -0,0 +1,23 @@
+// show new quote
+function newQuote() {
+  //   loading()
+  // pick a random quote from apiQuotes array
+  const quote = LocalQuotes[Math.floor(Math.random() * LocalQuotes.length)]
+  //   check if author field is blank and replace it with "unknown"
+  if (!quote.author) {
+    authorText.textContent = 'Unknown'
+  } else {
+    authorText.textContent = quote.author
+  }
+  // check quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote')
+  } else {
+    quoteText.classList.remove('long-quote')
+  }
+  //   set quote and hide loader
+  quoteText.textContent = quote.text
+  //   complete()
+}
+
+newQuote()
