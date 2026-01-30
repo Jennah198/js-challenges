142e921f912779f9cf97bed057463652105d4bae
Nezira <neziraworku198@gmail.com>
2026-01-30 11:45:44 -0800

    added a function that accepts photos from unsplash and set it to dom to get desplayed for users

--- 20-projects/Infinity-scroll/script.js
+++ 20-projects/Infinity-scroll/script.js
@@ -7,12 +7,31 @@ const count = 10
 const apiKey = 'FVF8lRGwREhsc45ZxCMOWZHk_DPCPNxr2rMad9kb9p0'
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`
 
+// create elements for links and photos, add to DOM
+function displayPhotos() {
+  // run func for each object in photosArray
+  photosArray.forEach((photo) => {
+    // create <a> to link to unsplash
+    const item = document.createElement('a')
+    item.setAttribute('href', photo.links.html)
+    item.setAttribute('target', '_blank')
+    // create <img> for photo
+    const img = document.createElement('img')
+    img.setAttribute('src', photo.urls.regular)
+    img.setAttribute('alt', photo.alt_description)
+    img.setAttribute('title', photo.alt_description)
+    // put <img> inside <a>, then put both inside imageContainer Element
+    item.appendChild(img)
+    imageContainer.appendChild(item)
+  })
+}
+
 // get photos from unsplash api
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl)
     photosArray = await response.json()
-    console.log(photosArray)
+    displayPhotos()
   } catch (error) {
     // catch error here
   }
