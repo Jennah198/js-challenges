2b3a6079313cf8c50b5f25acd812d7b017a985bf
Nezira <neziraworku198@gmail.com>
2026-01-30 04:26:26 -0800

    making the quotes come from external resource by including an api

--- 20-projects/Qute-Generator/script.js
+++ 20-projects/Qute-Generator/script.js
@@ -1,42 +1,54 @@
+let apiQuotes = []
 const quoteContainer = document.getElementById('quote-generator')
 const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
-const loader = document.getElementById('loader')
+// const loader = document.getElementById('loader')
 
-// show loading
-function loading() {
-  loader.hidden = false
-  quoteContainer.hidden = true
-}
+// // show loading
+// function loading() {
+//   loader.hidden = false
+//   quoteContainer.hidden = true
+// }
 
-// hide loading
-function complete() {
-  loader.hidden = true
-  quoteContainer.hidden = false
-}
+// // hide loading
+// function complete() {
+//   quoteContainer.hidden = false
+//   loader.hidden = true
+// }
 
 // show new quote
 function newQuote() {
-  //   loading()
-  // pick a random quote from apiQuotes array
-  const quote = LocalQuotes[Math.floor(Math.random() * LocalQuotes.length)]
-  //   check if author field is blank and replace it with "unknown"
-  if (!quote.author) {
-    authorText.textContent = 'Unknown'
-  } else {
-    authorText.textContent = quote.author
-  }
-  // check quote length to determine styling
+  if (apiQuotes.length === 0) return
+
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+
+  authorText.textContent = quote.author || 'Unknown'
+
   if (quote.text.length > 120) {
     quoteText.classList.add('long-quote')
   } else {
     quoteText.classList.remove('long-quote')
   }
-  //   set quote and hide loader
+
   quoteText.textContent = quote.text
-  //   complete()
+}
+
+// get Quotes from API
+async function getQuotes() {
+  const apiUrl = 'https://dummyjson.com/quotes/random'
+
+  try {
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+
+    // show the quote directly
+    quoteText.textContent = data.quote
+    authorText.textContent = data.author || 'Unknown'
+  } catch (error) {
+    console.log('no quotes', error)
+  }
 }
 
 // tweet quote
@@ -46,6 +58,8 @@ function tweetQuote() {
 }
 
 // event listners
-newQuoteBtn.addEventListener('click', newQuote)
+newQuoteBtn.addEventListener('click', getQuotes)
 twitterBtn.addEventListener('click', tweetQuote)
-newQuote()
+// newQuote()
+
+getQuotes()
