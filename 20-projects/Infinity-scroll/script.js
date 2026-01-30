1c33b763deb0436ed0cae41ec6723b9879c19868
Nezira <neziraworku198@gmail.com>
2026-01-30 12:02:26 -0800

    add a functionality that check to see if scrolling near bottom of page, load more photos

--- 20-projects/Infinity-scroll/script.js
+++ 20-projects/Infinity-scroll/script.js
@@ -53,4 +53,13 @@ async function getPhotos() {
   }
 }
 
+// check to see if scrolling near bottom of page, load more photos
+window.addEventListener('scroll', () => {
+  if (
+    window.innerHeight + window.scrollY >=
+    document.body.offsetHeight - 1000
+  ) {
+    getPhotos()
+  }
+})
 getPhotos()
