3f33c575a548a39089e2e32e9012cb7045682d1e
Nezira <neziraworku198@gmail.com>
2026-01-30 11:55:10 -0800

    added a helper function to set attributes on dom elements to avoid code repetition

--- 20-projects/Infinity-scroll/script.js
+++ 20-projects/Infinity-scroll/script.js
@@ -7,19 +7,35 @@ const count = 10
 const apiKey = 'FVF8lRGwREhsc45ZxCMOWZHk_DPCPNxr2rMad9kb9p0'
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`
 
+// helper function to set attributes on dom elements
+function setAttribute(elements, attributes) {
+  for (const key in attributes) {
+    elements.setAttribute(key, attributes[key])
+  }
+}
+
 // create elements for links and photos, add to DOM
 function displayPhotos() {
   // run func for each object in photosArray
   photosArray.forEach((photo) => {
     // create <a> to link to unsplash
     const item = document.createElement('a')
-    item.setAttribute('href', photo.links.html)
-    item.setAttribute('target', '_blank')
+    // item.setAttribute('href', photo.links.html)
+    // item.setAttribute('target', '_blank')
     // create <img> for photo
+    setAttribute(item, {
+      href: photo.links.html,
+      target: '_blank',
+    })
     const img = document.createElement('img')
-    img.setAttribute('src', photo.urls.regular)
-    img.setAttribute('alt', photo.alt_description)
-    img.setAttribute('title', photo.alt_description)
+    // img.setAttribute('src', photo.urls.regular)
+    // img.setAttribute('alt', photo.alt_description)
+    // img.setAttribute('title', photo.alt_description)
+    setAttribute(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    })
     // put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img)
     imageContainer.appendChild(item)
