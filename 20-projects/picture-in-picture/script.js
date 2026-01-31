c01135f01d9659ff87a6de86e10ee412f9816ad2
Nezira <neziraworku198@gmail.com>
2026-01-31 12:05:15 -0800

    added a fuction to selecct media and start streaming

---
+++ 20-projects/picture-in-picture/script.js
@@ -0,0 +1,18 @@
+const videoElement = document.getElementById('video')
+const button = document.getElementById('button')
+
+//prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia()
+    videoElement.srcObject = mediaStream
+    videoElement.onloadedmetadata = () => {
+      videoElement.play()
+    }
+  } catch (error) {
+    // catch error
+  }
+}
+
+// on load
+selectMediaStream()
